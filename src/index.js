be5b7bb059affb956714e459ef886ba01f6ade42
Allana Karla <allanakarla1702@gmail.com>
2022-01-25 15:50:03 -0300

    Modificando titulo do componete Header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Header from './components/Title';
+import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
