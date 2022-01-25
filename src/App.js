a79c43e69a9ad42c2217443b80920cf14d10d48e
Allana Karla <allanakarla1702@gmail.com>
2022-01-25 15:46:42 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from './components/Title';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (<Header />);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from './components/Title';
 
 ReactDOM.render(
   <React.StrictMode>
